b559132ce3b49b3f5d0a3348a1537c44e17842bc
Matthew Potts <dev.matthewpotts@gmail.com>
2021-03-28 13:34:30 -0600

    Removed modules and routing modules inside main components since the site will not be using lazy loading.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { MyInterestsComponent } from './interests/interests.component';
 import { HomeComponent } from './home/home.component';
 import { RecommendBooksComponent } from './recommend-books/recommend-books.component';
+import { ChessComponent } from './chess/chess.component';
+import { CreateGameComponent } from './chess/create-game/create-game.component';
+import { LobbyComponent } from './chess/lobby/lobby.component';
 
 const routes: Routes = [
   { path: 'hobbies-and-interests', component: MyInterestsComponent },
   { path: 'home', component: HomeComponent },
   { path: 'recommend-books', component: RecommendBooksComponent },
-  { path: '', redirectTo: '/home', pathMatch:'full'}
+  {
+    path: 'chess', component: ChessComponent,
+    children: [
+      { path: 'create-game', component: CreateGameComponent },
+      { path: 'join-game', component: LobbyComponent },
+      { path: '**', redirectTo: 'create-game'},
+    ]
+  },
+  // { path: '', redirectTo: '/home', pathMatch:'full'},
+  { path: '**', redirectTo: '/home', pathMatch:'full'}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,33 +3,75 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyInterestsModule } from 'src/app/interests/interests.module';
 import { MatButtonModule } from '@angular/material/button'
 import { MatRippleModule } from '@angular/material/core'
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { ChessModule } from 'src/app/chess/chess.module';
 import { RecommendBooksModule } from 'src/app/recommend-books/recommend-books.module';
 import { MatMenuModule } from '@angular/material/menu';
+import { CommonModule } from '@angular/common';
+import { ChessboardComponent } from './chess/chessboard/chessboard.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CreateGameComponent } from './chess/create-game/create-game.component';
+import { LobbyComponent } from './chess/lobby/lobby.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ChessComponent } from './chess/chess.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { PawnPromotionComponent } from './chess/pawn-promotion/pawn-promotion.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChessGameComponent, ConfirmResignDialog } from './chess/chess-game/chess-game.component';
+import { MyInterestsComponent } from './interests/interests.component';
+import { CarouselComponent } from 'src/app/shared/carousel/carousel.component';
+import { CinemaComponent } from './interests/cinema/cinema.component';
+import { VideoGamesComponent } from './interests/video-games/video-games.component';
+import { RouterModule } from '@angular/router';
+import { AstronomyComponent } from './interests/astronomy/astronomy.component';
+import { ScienceFictionComponent } from './interests/science-fiction/science-fiction.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ChessboardComponent,
+    LobbyComponent,
+    CreateGameComponent,
+    ChessComponent,
+    ChessGameComponent,
+    ConfirmResignDialog,
+    MyInterestsComponent,
+    CarouselComponent,
+    CinemaComponent,
+    VideoGamesComponent,
+    AstronomyComponent,
+    ScienceFictionComponent,
+    PawnPromotionComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MyInterestsModule,
-    ChessModule,
     MatRippleModule,
     MatCheckboxModule,
     MatButtonModule,
     HttpClientModule,
     MatMenuModule,
-    RecommendBooksModule
+    RecommendBooksModule,
+    DragDropModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatCardModule,
+    MatRadioModule,
+    MatInputModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
