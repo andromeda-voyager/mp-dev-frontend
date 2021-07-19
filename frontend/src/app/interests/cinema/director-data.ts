70b9d242564a2dcd2c6a6424d9b9f54c5b6991a1
Matthew Potts <dev.matthewpotts@gmail.com>
2021-07-19 10:33:08 -0600

    Removed some items so that the cinema component could be around the same height as the other interest components.

--- frontend/src/app/interests/cinema/director-data.ts
+++ frontend/src/app/interests/cinema/director-data.ts
@@ -5,10 +5,6 @@ export const DIRECTORS: FilmData[] = [
   { secondaryInfo: "Known for Seven Samurai", name: 'Akira Kurosawa', imdbLink: 'https://www.imdb.com/name/nm0000041/' },
   { secondaryInfo: "Known for The Tree of Life", name: 'Terrence Malick', imdbLink: 'https://www.imdb.com/name/nm0000517/' },
   { secondaryInfo: "Known for Arrival", name: 'Denis Villeneuve', imdbLink: 'https://www.imdb.com/name/nm0898288/' },
-  { secondaryInfo: "Known for The Dark Knight", name: 'Christopher Nolan', imdbLink: 'https://www.imdb.com/name/nm0634240/' },
   { secondaryInfo: "Known for Spirted Away", name: 'Hayao Miyazaki', imdbLink: 'https://www.imdb.com/name/nm0594503/' },
-  { secondaryInfo: "Known for Children of Men", name: "Alfonso Cuarón", imdbLink: "https://www.imdb.com/name/nm0190859/" },
-  { secondaryInfo: "Known for Blade Runner", name: "Ridley Scott", imdbLink: "https://www.imdb.com/name/nm0000631/" },
   { secondaryInfo: "Known for Parasite", name: "Bong Joon Ho", imdbLink: "https://www.imdb.com/name/nm0094435/" }
-
 ];
